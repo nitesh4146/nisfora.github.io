469d8cce6a21ffd26edbd81bbed00773b3a64eda
nitesh4146 <nitesh4146@gmail.com>
2022-01-03 13:38:11 -0500

    reconfig

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,115 @@
+var store = [{
+        "title": "Post: Modified Date",
+        "excerpt":"This post has been updated and should show a modified date if used in a layout. All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a...","categories": ["Blog"],
+        "tags": ["Post Formats","readability","standard"],
+        "url": "/blog/post-modified/",
+        "teaser": "/assets/images/box.jpg"
+      },{
+        "title": "Post: Standard",
+        "excerpt":"All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a garden, and she plucked another flower and ran with it to her mother. I suppose she...","categories": ["Blog"],
+        "tags": ["Post Formats","readability","standard"],
+        "url": "/blog/post-standard/",
+        "teaser": "/assets/images/box.jpg"
+      },{
+        "title": "Post: Chat",
+        "excerpt":"Abbott: Strange as it may seem, they give ball players nowadays very peculiar names. Costello: Funny names? Abbott: Nicknames, nicknames. Now, on the St. Louis team we have Who’s on first, What’s on second, I Don’t Know is on third– Costello: That’s what I want to find out. I want...","categories": ["Blog"],
+        "tags": ["chat","Post Formats"],
+        "url": "/blog/post-chat/",
+        "teaser": "/assets/images/box.jpg"
+      },{
+        "title": "Post: Notice",
+        "excerpt":"A notice displays information that explains nearby content. Often used to call attention to a particular detail. When using Kramdown {: .notice} can be added after a sentence to assign the .notice to the &lt;p&gt;&lt;/p&gt; element. Changes in Service: We just updated our privacy policy here to better service our...","categories": ["Blog"],
+        "tags": ["Post Formats","notice"],
+        "url": "/blog/post-notice/",
+        "teaser": "/assets/images/box.jpg"
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["blog"],
+        "tags": ["Jekyll","update"],
+        "url": "/blog/welcome-to-jekyll/",
+        "teaser": "/assets/images/mountains.jpg"
+      },{
+        "title": "Deep Neural Network from Scratch - Part 1",
+        "excerpt":"In this post I will be explaining how to design your own Neural Network (Keras-like) framework from scratch in python 3.7+. This post assumes you have basic understanding of how a Neural Network works and familiarity with forward-backward propagation, Loss functions, Optimizers, Regularization and so on. Without any further due,...","categories": ["blog"],
+        "tags": ["Jekyll","update"],
+        "url": "/blog/post-quote/",
+        "teaser": "https://unsplash.com/photos/842ofHC6MaI"
+      },{
+        "title": "7-step beginner’s guide to open-source contribution on GitHub",
+        "excerpt":"Photo by Yancy Min on Unsplash Contributing to open-source projects not only adds value to the project but also drives the entire community forward. Besides we all want to build a nice GitHub profile and earn those shiny contribution badges to showcase on profile. Let’s take an example that I...","categories": ["blog"],
+        "tags": ["Jekyll","update"],
+        "url": "/blog/post-link/",
+        "teaser": "/assets/images/mountains.jpg"
+      },{
+        "title": "Robot Path Planning with A*",
+        "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","categories": [],
+        "tags": [],
+        "url": "/projects/2017-04-01-a-star/",
+        "teaser": "https://media.giphy.com/media/iufZz12aYSJ2FZu5KL/giphy.gif"
+      },{
+        "title": "Q-Learning in Grid World",
+        "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","categories": [],
+        "tags": [],
+        "url": "/projects/2017-04-15-q-learning/",
+        "teaser": "https://media.giphy.com/media/hPmh1xtiaWH86yyqxd/giphy.gif"
+      },{
+        "title": "Autonomous Bot Using ROS",
+        "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","categories": [],
+        "tags": [],
+        "url": "/projects/2017-04-30-the-bot/",
+        "teaser": "https://media.giphy.com/media/a0V1icR8bN6GaWbqtM/giphy-downsized-large.gif"
+      },{
+        "title": "Lane-line Segmentation",
+        "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","categories": [],
+        "tags": [],
+        "url": "/projects/2017-07-20-lane-line/",
+        "teaser": "https://media.giphy.com/media/6y2CDkejojfOBt68ff/giphy-downsized-large.gif"
+      },{
+        "title": " Drivers Behavior Cloning",
+        "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","categories": [],
+        "tags": [],
+        "url": "/projects/2017-08-09-behavior/",
+        "teaser": "https://media.giphy.com/media/HizQGy8t7tjqhl3gww/giphy.gif"
+      },{
+        "title": "Traffic Sign Classification",
+        "excerpt":"Load Data Load the MNIST data, which comes pre-loaded with TensorFlow. # Load pickled data import pickle import numpy as np training_file = 'train.p' validation_file = 'valid.p' testing_file = 'test.p' with open(training_file, mode='rb') as f: train = pickle.load(f) with open(validation_file, mode='rb') as f: valid = pickle.load(f) with open(testing_file, mode='rb') as...","categories": [],
+        "tags": [],
+        "url": "/projects/2017-09-01-traffic-sign/",
+        "teaser": "https://media.giphy.com/media/XFs5aeqmQGKgECnuVd/giphy.gif"
+      },{
+        "title": "Vehicle Detection and Tracking",
+        "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","categories": [],
+        "tags": [],
+        "url": "/projects/2017-09-10-vehicle-detection/",
+        "teaser": "https://media.giphy.com/media/iHCMBbXZJ9jnDeSg1F/giphy-downsized-large.gif"
+      },{
+        "title": "Connected Automated Vehicle Emulator",
+        "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","categories": [],
+        "tags": [],
+        "url": "/projects/2018-07-03-cavs/",
+        "teaser": "https://media.giphy.com/media/yJuv5mtwv4TkukL61l/giphy.gif"
+      },{
+        "title": "Vanilla Neural Network",
+        "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","categories": [],
+        "tags": [],
+        "url": "/projects/2018-07-30-vanilla-nn/",
+        "teaser": "https://media.giphy.com/media/16quBBr5bnK8kdOAVy/giphy.gif"
+      },{
+        "title": "Intelligent Industrial Automation",
+        "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","categories": [],
+        "tags": [],
+        "url": "/projects/2020-07-30-panda/",
+        "teaser": "https://media.giphy.com/media/qenjoJXAJ3wpgeobdE/giphy.gif"
+      },{
+        "title": "End-to-end Learning in SVL simulator",
+        "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","categories": [],
+        "tags": [],
+        "url": "/projects/2021-04-30-svl/",
+        "teaser": "https://media.giphy.com/media/STVcFFvJ7cC0C88xja/giphy-downsized-large.gif"
+      },{
+        "title": "F1Tenth Autonomous Racing",
+        "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","categories": [],
+        "tags": [],
+        "url": "/projects/2021-09-10-f1tenth/",
+        "teaser": "https://media.giphy.com/media/JgygOfVwgkyujvJhpU/giphy-downsized-large.gif"
+      }]
